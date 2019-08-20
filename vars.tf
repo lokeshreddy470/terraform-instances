6bcb204b8323b3238eb8ed06dfe2c620eee3669f
abhisheksanga <abhisheksanga66@gmail.com>
2019-08-19 19:32:50 -0500

    Adding some changes

--- vars.tf
+++ vars.tf
@@ -1,11 +1,21 @@
-variable "key_pair" {
-  default = ""
+variable "key_name" {
+  default = "Nihar"
 }
 
 variable "general_instance_count" {
-  default = 2
+  default = 1
 }
 
 variable "general_instance_type" {
-  default = "t3.large"
+  default = "t2.micro"
 }
+
+variable "ami" {
+  default = "ami-035b3c7efe6d061d5" 
+}
+
+variable "vpc_id" {
+  default = "vpc-ba9e00c0"
+}
+
+
